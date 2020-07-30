f9ecbf3fab36063a7233933ca9791b5672ddd865
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-07-29 23:03:39 -0600

    updated db files

--- db/schema.sql
+++ db/schema.sql
@@ -26,6 +26,32 @@ CREATE TABLE employee (
   PRIMARY KEY (id)
 );
 
+-- Main Query --
+SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, 
+CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  LEFT JOIN employee m
+	ON m.id = e.manager_id;
+
+  -- view by department --
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee 
+LEFT JOIN role ON employee.role_id = role.id 
+LEFT JOIN department ON department.id = role.department_id 
+LEFT JOIN employee m ON m.id = employee.manager_id WHERE department.id =1;
+
+-- BUDGET --
+SELECT department_id,
+    department.name AS department,
+    SUM(salary) totalSalary
+    FROM
+    department 
+INNER JOIN role
+	ON department.id=role.department_id
+    WHERE department.id =?
 
 SELECT * FROM department;
 SELECT * FROM role;
--- db/seed.sql
+++ db/seed.sql
@@ -1,39 +1,51 @@
 INSERT INTO department (name)
-VALUES ("SALES"), ("WAREHOUSE"), ("CUSTOMER SERVICE"), ("ACCOUNTING"), ("RECEPTION"), ("HUMAN RESOURCES");
+VALUES ("SALES"), 
+("WAREHOUSE"), 
+("CUSTOMER SERVICE"), 
+("ACCOUNTING"), 
+("RECEPTION"), 
+("HUMAN RESOURCES"), 
+("MANAGEMENT"), 
+("OFFICE STAFF");
 
--- '1','SALES'
--- '2','WAREHOUSE'
--- '3','CUSTOMER SERVICE'
--- '4','ACCOUNTING'
--- '5','RECEPTION'
--- '6','HUMAN RESOURCES'
--- '7', 'MANAGEMENT'
--- '8','OFFICE STAFF'
-
-
-
+-- ROLE TABLE --
 INSERT INTO role (title, salary, department_id)
-VALUES ("SR Salesman", 80000, 1), ("Foreman", 50000, 2), ("JR Salesman", 60000, 1), ("Loader", 40000, 2), ("Customer Service Rep", 65000, 3), ("SR Accountant", 80000, 4), ("Accountant", 70000, 4), ("Receptionist", 40000, 5), ("HR Manager", 85000, 6), ("HR Generalist", 55000, 6), ("Receptionist", 40000, 5), ("Regional Manager", 90000, 7), ("Assistant to the Regional Manager", 80000, 7), ("Coorporate Manager", 100000, 7);
+VALUES ("SR Salesman", 80000, 1), 
+("Foreman", 50000, 2), 
+("JR Salesman", 60000, 1), 
+("Loader", 40000, 2), 
+("Customer Service Rep", 65000, 3), 
+("SR Accountant", 80000, 4), 
+("Accountant", 70000, 4), 
+("Receptionist", 40000, 5), 
+("HR Manager", 85000, 6), 
+("HR Generalist", 55000, 6), 
+("Receptionist", 40000, 5), 
+("Regional Manager", 90000, 7), 
+("Asst. to the Regional Manager", 80000, 7), 
+("Coorporate Manager", 100000, 7), 
+("Vice President", "150000", 7), 
+("Office Staff", 40000, 8);
 
--- '1','SR Salesman','80000.00','1'
--- '2','Foreman','50000.00','2'
--- '3','JR Salesman','60000.00','1'
--- '4','Loader','40000.00','2'
--- '5','Customer Service Rep','65000.00','3'
--- '6','SR Accountant','80000.00','4'
--- '7','Accountant','70000.00','4'
--- '8','Receptionist','40000.00','5'
--- '9','HR Manager','85000.00','6'
--- '10','HR Generalist','55000.00','6'
--- '11','Receptionist','40000.00','5'
--- '12','Regional Manager','90000.00','7'
--- '13','AssT. to the Regional Manager','80000.00','7'
--- '14','Coorporate Manager','100000.00','7'
--- '15','CEO','120000.00','7'
--- '16','Temp','40000.00','8'
--- '17','Office Staff','40000.00','8'
-
-
-
-INSERT INTO employee (first_name, last_name, role_id, department_id) 
-VALUES ("Jim", "Halpert", 1, 1), ("Dwight", "Schrute", 13, 7), ("Andy", "Bernard", 1, 1), ("Stanley", "Hudson", 1, 1), ("Phylis", "Vance", 1, 1), ("Pam", "Beesly", 8, 5),  ("Ryan", "Howard", 16, 8), ("Ryan", "Howard", 16, 8), ("Michael", "Scott", 12, 7), ("Jan", "Levinson", 14, 7), ("Kevin", "Malone", 7, 4), ("Angela", "Martin", 6, 4), ("Oscar", "Martinez", 6, 4), ("Meredith", "Palmer", 17, 8), ("Creed", "Bratton", 17, 8), ("Toby", "Flenderson", 10, 6), ("Holly", "Flax", 9, 8), ("Kelly", "Kapoor", 5, 3), ("Darryl", "Philbin", 2, 2), ("Roy", "Anderson", 4, 2);
+-- EMPLOYEE TABLE --
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ("Michael", "Scott", 12, 7), 
+("David", "Wallace",15 , 2),
+ ("Jan", "Levinson", 14, 2),
+("Jim", "Halpert",1, 1), 
+("Dwight", "Schrute", 13, 1), 
+("Andy", "Bernard", 1, 1),  
+("Stanley", "Hudson", 1, 1), 
+("Phylis", "Vance", 1, 1), 
+("Pam", "Beesly", 8, 1),  
+("Ryan", "Howard", 16, 1), 
+("Kevin", "Malone", 7, 1), 
+("Angela", "Martin", 6, 1), 
+("Oscar", "Martinez", 6, 1), 
+("Meredith", "Palmer", 17, 1), 
+("Creed", "Bratton", 17, 1), 
+("Toby", "Flenderson", 10, 2), 
+("Holly", "Flax", 9, 2), 
+("Kelly", "Kapoor", 5, 18), 
+("Darryl", "Philbin", 2, 1), 
+("Roy", "Anderson", 4, 19);
